01d46a65c29cf3000fc98bee0d07bb1845af8465
acraven <andrew@epworth-consulting.co.uk>
2016-11-28 21:30:06 +0000

    Compose load test

---
+++ load-test.sh
@@ -0,0 +1,14 @@
+ip=$(docker-machine ip)
+
+docker-compose up --build -d
+
+ab.exe -n 20 http://$ip:8110/ping > /dev/null
+ab.exe -n 40000 -c 20 http://$ip:8110/ > results/nodejs.txt 
+
+ab.exe -n 20 http://$ip:8105/ping > /dev/null
+ab.exe -n 40000 -c 20 http://$ip:8105/ > results/aspnetcore-mvc.txt 
+
+ab.exe -n 20 http://$ip:8120/ping > /dev/null
+ab.exe -n 40000 -c 20 http://$ip:8120/ > results/aspnetcore-http.txt 
+
+docker-compose down
