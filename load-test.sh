d243c5d75972a8202d18de5471a2c02644ffb095
acraven <andrew@epworth-consulting.co.uk>
2016-11-28 22:07:53 +0000

    Compose mono owin

--- load-test.sh
+++ load-test.sh
@@ -11,4 +11,7 @@ ab.exe -n 40000 -c 20 http://$ip:8105/ > results/aspnetcore-mvc.txt
 ab.exe -n 20 http://$ip:8120/ping > /dev/null
 ab.exe -n 40000 -c 20 http://$ip:8120/ > results/aspnetcore-http.txt 
 
+ab.exe -n 20 http://$ip:8130/ping > /dev/null
+ab.exe -n 40000 -c 20 http://$ip:8130/ > results/mono-owin.txt 
+
 docker-compose down
---
+++ mono-owin/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace Api.Benchmark.Mono.Owin
+{
+   using System.Threading;
+
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         var url = "http://*:8130";
+
+         Console.CancelKeyPress += (sender, eventArgs) =>
+         {
+            Console.WriteLine("exiting...");
+         };
+
+         using (WebApp.Start<Startup>(url))
+         {
+            Console.WriteLine("Running on {0}", url);
+            Console.WriteLine("Press Ctrl-C to exit");
+            Thread.Sleep(Timeout.Infinite);
+         }
+      }
+   }
+
+   public class Startup
+   {
+      public void Configuration(IAppBuilder app)
+      {
+         app.Map("/ping", builder =>
+         {
+            builder.Run(context =>
+            {
+               context.Response.ContentType = "text/plain";
+               return context.Response.WriteAsync("Pong!");
+            });
+         });
+      }
+   }
+}
