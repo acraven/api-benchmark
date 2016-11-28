13fcf1d2a54f7d351365554ddbfcf38a0c5cbb61
acraven <andrew@epworth-consulting.co.uk>
2016-11-28 22:55:30 +0000

    Tweaks for linux

--- load-test.sh
+++ load-test.sh
@@ -1,23 +1,28 @@
-rm -f results
+rm -f -r results
 mkdir results
 
 ip=$(docker-machine ip)
+if [ "$?" -ne "0" ]; then
+  ip="localhost"
+fi
+
+echo "Using $ip"
 
 docker-compose up --build -d
 
-ab.exe -n 20 http://$ip:8110/ping > /dev/null
-ab.exe -n 40000 -c 20 http://$ip:8110/ > results/nodejs.txt 
+ab -n 20 http://$ip:8110/ping > /dev/null
+ab -n 40000 -c 20 http://$ip:8110/ping > results/nodejs.txt 
 
-ab.exe -n 20 http://$ip:8105/ping > /dev/null
-ab.exe -n 40000 -c 20 http://$ip:8105/ > results/aspnetcore-mvc.txt 
+ab -n 20 http://$ip:8105/ping > /dev/null
+ab -n 40000 -c 20 http://$ip:8105/ping > results/aspnetcore-mvc.txt 
 
-ab.exe -n 20 http://$ip:8120/ping > /dev/null
-ab.exe -n 40000 -c 20 http://$ip:8120/ > results/aspnetcore-http.txt 
+ab -n 20 http://$ip:8120/ping > /dev/null
+ab -n 40000 -c 20 http://$ip:8120/ping > results/aspnetcore-http.txt 
 
-ab.exe -n 20 http://$ip:8130/ping > /dev/null
-ab.exe -n 40000 -c 20 http://$ip:8130/ > results/mono-owin.txt 
+ab -n 20 http://$ip:8130/ping > /dev/null
+ab -n 40000 -c 20 http://$ip:8130/ping > results/mono-owin.txt 
 
-ab.exe -n 20 http://$ip:8140/ping > /dev/null
-ab.exe -n 40000 -c 20 http://$ip:8140/ > results/mono-nancy.txt 
+ab -n 20 http://$ip:8140/ping > /dev/null
+ab -n 40000 -c 20 http://$ip:8140/ping > results/mono-nancy.txt 
 
 docker-compose down
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-api-benchmark
+## api-benchmark
+
+If necessary, install Apache Benchmark (`ab.exe`) using `sudo apt-get install apache2-utils`
+
+Run `load-test.sh` to build, run and load test all containers
